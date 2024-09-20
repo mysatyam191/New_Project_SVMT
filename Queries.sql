385b7e1d7254ed9ac1d2d5618f2aa34862a5b3f2
mysatyam191 <121924000+mysatyam191@users.noreply.github.com>
2024-09-20 16:34:28 +0530

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,9 @@
+INSERT INTO `role`
+(id, description, details)
+VALUES(1, 'ROLE_ADMIN', 'Admin Role');
+INSERT INTO `role`
+(id, description, details)
+VALUES(2, 'ROLE_USER', 'User Role');
+INSERT INTO `role`
+(id, description, details)
+VALUES(3, 'ROLE_VOLUNTEER', 'Volunteer Role');
